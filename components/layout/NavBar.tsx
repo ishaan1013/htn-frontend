be15575603b1593b7db6a22c1f6c839973ddf02a
Ishaan Dey <ishaan1013@gmail.com>
2024-02-08 00:13:19 -0500

    init project

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "@/assets/logo.svg"
+import { Button } from "../ui/button"
+import { ArrowRight, LogIn } from "lucide-react"
+
+export default function NavBar() {
+  return (
+    <div className="w-screen flex select-none justify-center">
+      <nav className="w-full max-w-screen-lg p-6 flex items-center justify-between">
+        <Link href="/" className="w-10 h-10">
+          <Image alt="Logo" src={Logo} height={40} width={40} />
+        </Link>
+        <Button variant="outline">Sign In</Button>
+      </nav>
+    </div>
+  )
+}
