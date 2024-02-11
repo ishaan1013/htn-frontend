2ced5e98702242f9e870d2d80082149c0721a429
Ishaan Dey <ishaan1013@gmail.com>
2024-02-11 17:38:28 -0500

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import NavBar from "@/components/layout/NavBar"
 import { ThemeProvider } from "@/components/layout/ThemeProvider"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
 export const metadata: Metadata = {
   title: "Hack the North",
   description: "Ishaan's frontend take-home assignment for HTN.",
 }
+
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export default function RootLayout({
   children,
