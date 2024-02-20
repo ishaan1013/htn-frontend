b5caca26f70a7fa49468d2d85af314896728064a
Ishaan Dey <ishaan1013@gmail.com>
2024-02-19 22:39:30 -0500

    import error fix?

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Schibsted_Grotesk } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
-import NavBar from "@/components/layout/NavBar"
+import NavBar from "@/components/layout/navBar"
 import { ThemeProvider } from "@/components/layout/ThemeProvider"
 import Footer from "@/components/layout/footer"
 
